4b6ebdc5f45f64934b5a64558bed6bc82de4efae
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-23 12:37:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,11 +8,11 @@ def directors_totals(nds)
   
   while director_index < nds.length do 
     directors_name = nds[director_index][:name]
-    totals[director_name] = 0 
+    totals[directors_name] = 0 
     movie_index = 0 
     
     while movie_index < nds[director_index][:movies].length do 
-      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      totals[directors_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
   end 
    director_index += 1
